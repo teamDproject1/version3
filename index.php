81d61a3ac3c00e40a5fe901d042f188eadfa93a6
naresh <janagam.naresh@gmail.com>
2014-10-15 15:13:33 +1100

    index.php udate

---
+++ index.php
@@ -0,0 +1,312 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ICT ERA</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/flaticon.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	<div class="row head">
+		<h1 class="text-center h1">
+			<img src="logo.gif">ICT ERA
+			<span class="show_mobile">
+				<i class="flaticon-menu48"></i>&nbsp;&nbsp;<b class="hide_me">Menu</b>
+			</span>
+		</h1>
+	</div>
+    <div class="container">
+		<nav class="navbar navbar-default hide_mobile" role="navigation">
+        	<div class="container-fluid">
+          		<div class="navbar-header">
+            		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+            		</button>
+          		</div>
+          		<div id="navbar" class="navbar-collapse collapse">
+            		<ul class="nav navbar-nav">
+						<li class="active"><a href="index.html">Home</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Who are you?<span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li>
+									<a href="parent.html" title="View all posts filed under Development">parents</a>
+								</li>
+								<li>
+									<a href="teacher.html" title="View all posts filed under Market research">teachers</a>
+								</li>
+								<li>
+									<a href="student.html" title="View all posts filed under Security">students</a>
+								</li>
+								<li>
+									<a href="careeradvisors.html" title="View all posts filed under Software licensing">career advisors</a>
+								</li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Archives<span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li>
+									<a href='index.html' title='October 2007'>Sucessfull Stories</a>
+								</li>
+								<li>
+									<a href='index.html' title='October 2007'>Case Studies</a>
+								</li>
+							</ul>
+						</li>
+						<li><a href="#">About Us</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Opportunities For Women<span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li>
+									<a href="index.html">Ict Courses</a>
+								</li>
+								<li>
+									<a href="index.html">Providers</a>
+								</li>
+								<li>
+									<a href="index.html">Scholarships</a>
+								</li>
+								<li>
+									<a href="index.html">Internships</a>
+								</li>
+								<li>
+									<a href="index.html">Jobs</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+         		</div><!--/.nav-collapse -->
+        	</div><!--/.container-fluid -->
+      	</nav>
+
+		<!-- <div class="row">
+			<div class="well well-lg">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Input Your Interests</h3>
+					</div>
+					<div class="panel-body">
+						<form method="get" id="searchform" action="home.html">
+							<div class="col-xs-8">
+	    						<input type="text" class="form-control" placeholder="Search">
+	  						</div>
+	  						<button type="button" class="btn btn-default active">Search</button>
+	  					</form>
+					</div>
+				</div>
+			</div>
+		</div> -->
+		<div class="row">
+			<div class="col-sm-5 col-md-3 menu-warp">
+				<div class="row text-right">
+					<span class="btn btn-default"><i class="flaticon-close40"></i>&nbsp;&nbsp;close</span>
+				</div>
+				<div class="panel-group" id="accordion">
+				  	<div class="panel panel-default">
+				    	<div class="panel-heading">
+							<h4 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+									Who are you?
+								</a>
+							</h4>
+				    	</div>
+				    	<div id="collapseOne" class="panel-collapse collapse in">
+				      		<div class="panel-body">
+				        		<ul class="nav nav-pills nav-stacked">
+									<li>
+										<a href="parent.html" title="View all posts filed under Development">parents</a>
+									</li>
+									<li>
+										<a href="teacher.html" title="View all posts filed under Market research">teachers</a>
+									</li>
+									<li>
+										<a href="student.html" title="View all posts filed under Security">students</a>
+									</li>
+									<li>
+										<a href="careeradvisors.html" title="View all posts filed under Software licensing">career advisors</a>
+									</li>
+								</ul>
+				      		</div>
+				    	</div>
+				  	</div>
+				  	<div class="panel panel-default">
+				    	<div class="panel-heading">
+					     	<h4 class="panel-title">
+					        	<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+					          		Archives
+					        	</a>
+					      	</h4>
+				    	</div>
+						<div id="collapseTwo" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul class="nav nav-pills nav-stacked">
+	  								<li>
+										<a href='home.html' title='October 2007'>Sucessfull Stories</a>
+									</li>
+									<li>
+										<a href='home.html' title='October 2007'>Case Studies</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+				  </div>
+				  <div class="panel panel-default">
+				    	<div class="panel-heading">
+					      	<h4 class="panel-title">
+					        	<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+					          		About Us
+					        	</a>
+					      	</h4>
+				    	</div>
+					    <div id="collapseThree" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul class="nav nav-pills nav-stacked">
+	  	
+								</ul>
+							</div>
+					    </div>
+				  	</div>
+				  	<div class="panel panel-default">
+				    	<div class="panel-heading">
+					      	<h4 class="panel-title">
+					        	<a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+					          		Our Clients
+					        	</a>
+					      	</h4>
+				    	</div>
+					    <div id="collapseFour" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul class="nav nav-pills nav-stacked">
+	  								<li>
+										<a href="http://www.ibm.com.au/">IBM</a>
+									</li>
+									<li>
+										<a href="http://www.cisco.com/web/ANZ">CISCO</a>
+									</li>
+									<li>
+										<a href="http://www.colescareers.com.au/">COLES</a>
+									</li>
+									<li>
+										<a href="http://www.wowcareers.com.au/">WOOLWORTHS</a>
+									</li>
+								</ul>
+							</div>
+					    </div>
+				  	</div>
+				  	<div class="panel panel-default">
+				    	<div class="panel-heading">
+					      	<h4 class="panel-title">
+					        	<a data-toggle="collapse" data-parent="#accordion" href="#collapseFive">
+					          		Opportunities For Women
+					        	</a>
+					      	</h4>
+				    	</div>
+					    <div id="collapseFive" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul class="nav nav-pills nav-stacked">
+	  								<li>
+										<a href="home.html">ICT courses</a>
+									</li>
+									<li>
+										<a href="home.html">PROVIDERS</a>
+									</li>
+									<li>
+										<a href="home.html">SCHOLARSHIPS</a>
+									</li>
+									<li>
+										<a href="home.html">INTERNSHIPS</a>
+									</li>
+									<li>
+										<a href="home.html">JOBS</a>
+									</li>
+								</ul>
+							</div>
+					    </div>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-12 page-warp">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							<a href="http://osc3.template-help.com/wordpress_16693/?p=4" rel="bookmark" title="Permanent Link to Apache guru praises Vista">WHY ICT?</a>
+						</h3>
+					</div>
+					<div class="panel-body">
+						<p>
+							Information and Communications Technologies (ICT) education is basically our society’s efforts to teach its current and emerging citizens valuable knowledge and skills around computing and communications devices, software that operates them, applications that run on them and systems that are built with them.<br>
+							ICT is complex and quickly changing, and it is confusing for many people. It is so pervasive in the modern world that everyone has some understanding of it, but those understandings are often wildly divergent.
+						</p>
+					</div>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							<a href="http://osc3.template-help.com/wordpress_16693/?p=3" rel="bookmark">Suggestions</a>
+						</h3>
+					</div>
+					<div class="panel-body">
+						<ul class="list-group col-xs-4">
+							<li class="list-group-item">
+								<a href="ict courses.html">ICT courses </a>
+							</li>
+							<li class="list-group-item">
+								<a href="coursesondemand.html">Courses on demand</a>
+							</li>
+							<li class="list-group-item">
+								<a href="instcourses.html">Institutions offering ICT courses</a>
+							</li>
+						</ul>
+						<ul class="list-group col-xs-4">
+							<li class="list-group-item">
+								<a href="https://www.youtube.com/watch?v=raj_W47SZoc">what Parent say ?</a>
+							</li>
+							<li class="list-group-item">
+								<a href="https://www.youtube.com/watch?v=NqXdBX0jF9U">what Career advisor say? ?</a>
+							</li>
+							<li class="list-group-item">
+								<a href="https://www.youtube.com/watch?v=2gvthKd7O-0">Why Computer Science & ICT?</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row clients-wrap">
+			<h1 class="text-center">Our Clients</h1>
+			<img src="img/ibm.png" class="col-xs-6 col-md-3">
+			<img src="img/cisco.jpg" class="col-xs-6 col-md-3">
+			<img src="img/coles.jpg" class="col-xs-6 col-md-3">
+			<img src="img/woolworths.png" class="col-xs-6 col-md-3">
+		</div>
+	</div>
+	<footer class="row">
+		<h3 class="text-center">
+			<a href='http://www.facebook.com' ><img src='fb.jpeg'/></a> 
+			<a href='http://www.twitter.com' ><img src='twitter.png'/></a> 
+			<a href='http://www.linkedin.com' ><img src='Linkedin.png'/></a> 
+		</h3>
+	</footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
